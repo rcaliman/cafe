10921304162cfbb174ba40c89b7a093594a8a0f8
Rodrigo Caliman <rcaliman@gmail.com>
2021-07-13 09:26:33 -0300

    tratamento de erros

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def inicio():
         usuario = session['usuario']
         return render_template('inicio.html',
                                usuario = usuario)
-    except:
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return redirect('/form_login',302)
 
 @app.route('/form_login') # formulário de login
@@ -55,26 +56,31 @@ def cafes():
                 asc_desc = asc_desc,
             )
     except Exception:
-        logging.exception('Erro')
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 @app.route('/form_cafe') #formulario para fazer o cadastro dos cafés
 def form_cafe():
     try:
         if logado():
-            if request.values.get('id'):
-                id = int(request.values.get('id'))
-                cafe = select_cafe(id)
-                return render_template(
-                    'form_cafe.html',
-                    id = id,
-                    cafe = cafe,
-                )
-            else:
-                return render_template(
-                    'form_cafe.html',
-                )
-    except:
+            try:
+                if request.values.get('id'):
+                    id = int(request.values.get('id'))
+                    cafe = select_cafe(id)
+                    return render_template(
+                        'form_cafe.html',
+                        id = id,
+                        cafe = cafe,
+                    )
+                else:
+                    return render_template(
+                        'form_cafe.html',
+                    )
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return cafes()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
 
 @app.route('/insere_cafe',methods=['GET','POST']) # action do formulario de cadastro de cafés
@@ -95,86 +101,106 @@ def insere_cafe():
                     cafe.insere_banco()
                     return redirect('/cafes',302)
                 except Exception:
-                    logging.exception('Erro')
+                    logging.exception('ERRO APP CAFES E TORRRAS')
                     return form_cafe()
     except Exception:
-        logging.exception('Erro')
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 @app.route('/update_cafe',methods=['GET','POST']) # action do formulário de atualizacao de cafés
 def update_cafe():
     try:
         if logado():
-            if request.form:
-                cafe = Cafe(
-                    request.form['id'],
-                    request.form['descricao'],
-                    request.form['quantidade'],
-                    request.form['data_compra'],
-                    request.form['origem'],
-                    request.form['estoque'],
-                    usuario_id(session['usuario'])
-                )
-                cafe.update_cafe()
-        return redirect('/cafes',302)
-    except:
+            try:
+                if request.form:
+                    cafe = Cafe(
+                        request.form['id'],
+                        request.form['descricao'],
+                        request.form['quantidade'],
+                        request.form['data_compra'],
+                        request.form['origem'],
+                        request.form['estoque'],
+                        usuario_id(session['usuario'])
+                    )
+                    cafe.update_cafe()
+                    return redirect('/cafes',302)
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return cafes()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
 
 @app.route('/apaga_cafe', methods=['GET','POST']) # action para apagar um café do banco
 def apaga_cafe():
     try:
         if logado():
-            id = int(request.values.get('id'))
-            apagar_cafe(id)
-        return redirect('/cafes',302)
-    except:
+            try:
+                id = int(request.values.get('id'))
+                apagar_cafe(id)
+                return redirect('/cafes',302)
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return cafes()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
 
 @app.route('/torras', methods=['GET','POST']) # tabela com torras já feitas
 def torras():
     try:
         if logado():
-            ord = request.values.get('ord') or 'id'
-            asc = request.values.get('asc_desc') or 'desc'
-            usuario = usuario_id(session['usuario'])
-            select = html_select_id()
-            if request.values.get('asc_desc') == 'asc':
-                asc_desc = 'desc'
-            else:
-                asc_desc = 'asc'
-            return render_template(
-                'torras.html',
-                torras = select_torras(ord,asc, usuario),
-                descricao_cafes = busca_descricao_cafes(),
-                ord = ord,
-                asc_desc = asc_desc,
-                select = select,
-            )
-    except:
+            try:
+                ord = request.values.get('ord') or 'id'
+                asc = request.values.get('asc_desc') or 'desc'
+                usuario = usuario_id(session['usuario'])
+                select = html_select_id()
+                if request.values.get('asc_desc') == 'asc':
+                    asc_desc = 'desc'
+                else:
+                    asc_desc = 'asc'
+                return render_template(
+                    'torras.html',
+                    torras = select_torras(ord,asc, usuario),
+                    descricao_cafes = busca_descricao_cafes(),
+                    ord = ord,
+                    asc_desc = asc_desc,
+                    select = select,
+                )
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return inicio()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 @app.route('/torra', methods=['GET','POST']) # tabela com os dados de uma torra específica
 def torra():
     try:
         if logado():
-            id = request.values.get('id')
-            torra = select_torra(id)
-            id_cafe = torra[0][1]
-            descricao_cafe = busca_descricao_cafe(id_cafe) 
-            grid_torra = json.loads(json_torras(id)[0][0])
-            path_grafico = 'static/img/grafico.png'
-            plot = grafico_torras(json_torras(id),path_grafico, 0)
-            randomico = random.randrange(1,1000)
-            return render_template(
-                'torra.html',
-                torra = torra,
-                grid_torra = grid_torra,
-                plot = plot,
-                id = id,
-                descricao_cafe = descricao_cafe[0][0],
-                randomico = randomico,
-            )
-    except:
+            try:
+                id = request.values.get('id')
+                torra = select_torra(id)
+                id_cafe = torra[0][1]
+                descricao_cafe = busca_descricao_cafe(id_cafe) 
+                grid_torra = json.loads(json_torras(id)[0][0])
+                path_grafico = 'static/img/grafico.png'
+                plot = grafico_torras(json_torras(id),path_grafico, 0)
+                randomico = random.randrange(1,1000)
+                return render_template(
+                    'torra.html',
+                    torra = torra,
+                    grid_torra = grid_torra,
+                    plot = plot,
+                    id = id,
+                    descricao_cafe = descricao_cafe[0][0],
+                    randomico = randomico,
+                )
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return torras()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 @app.route('/form_torra', methods=['GET','POST']) # formulário para cadastrar uma torra
@@ -188,102 +214,120 @@ def form_torra():
                     js = js_torra(ids),                      
                 )
             except Exception:
-                logging.exception('Erro')
+                logging.exception('ERRO APP CAFES E TORRRAS')
                 return torras()
-    except:
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
 
 @app.route('/insere_torra', methods=['GET','POST']) # action do formulário de inserir nova torra
 def insere_torra():
     try:
         if logado():
-            if request.form:
-                torra = Torra(
-                    None,
-                    request.form['cafe'],
-                    request.form['temp_inicial'],
-                    request.form['temp_final'],
-                    request.form['piso'],
-                    monta_temp_json(request.form),
-                    request.form['fluxo_ar'],
-                    request.form['velocidade_tambor'],
-                    request.form['peso'],
-                    request.form['data'],
-                    request.form['observacoes'],
-                    usuario_id(session['usuario']))
-                torra.insere_banco()
-                
-        return redirect('/torras',302)
-    except:
-            return form_login()
+            try:
+                if request.form:
+                    torra = Torra(
+                        None,
+                        request.form['cafe'],
+                        request.form['temp_inicial'],
+                        request.form['temp_final'],
+                        request.form['piso'],
+                        monta_temp_json(request.form),
+                        request.form['fluxo_ar'],
+                        request.form['velocidade_tambor'],
+                        request.form['peso'],
+                        request.form['data'],
+                        request.form['observacoes'],
+                        usuario_id(session['usuario']))
+                    torra.insere_banco()
+                    return redirect('/torras',302)
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return torras()    
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
+        return form_login()
 
 @app.route('/edita_torra') # formulário para editar uma torra já feita
 def edita_torra():
     try:
         if logado():
-            id = int(request.values.get('id'))
-            torra = select_torra(id)
-            id_cafe = torra[0][1]
-            grid_torra = edit_gridform(id)
-            select_cafes = select_descricao_cafes(id_cafe)
-            return render_template('edita_torra.html',
-                torra = torra,  
-                grid_torra = grid_torra,  
-                select_cafes = select_cafes,        
-            )
+            try:
+                id = int(request.values.get('id'))
+                torra = select_torra(id)
+                id_cafe = torra[0][1]
+                grid_torra = edit_gridform(id)
+                select_cafes = select_descricao_cafes(id_cafe)
+                return render_template('edita_torra.html',
+                    torra = torra,  
+                    grid_torra = grid_torra,  
+                    select_cafes = select_cafes,        
+                )
+            except:
+                return torras()
     except Exception:
-        logging.exception('Erro')
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 @app.route('/update_torra', methods=['GET','POST']) # action para atualizar os dados de uma torra
 def update_torra():
     try:
         if logado():
-            if request.form:
-                torra = Torra(
-                    request.form['id'],
-                    request.form['cafe'],
-                    request.form['temp_inicial'],
-                    request.form['temp_final'],
-                    request.form['piso'],
-                    monta_temp_json(request.form),
-                    request.form['fluxo_ar'],
-                    request.form['velocidade_tambor'],
-                    request.form['peso'],
-                    request.form['data'],
-                    request.form['observacoes'])
-                torra.update_torra()
-                url = '/torra?id=' + request.form['id']
-        return redirect(url, 302)
-    except:
-            return form_login()
+            try:
+                if request.form:
+                    torra = Torra(
+                        request.form['id'],
+                        request.form['cafe'],
+                        request.form['temp_inicial'],
+                        request.form['temp_final'],
+                        request.form['piso'],
+                        monta_temp_json(request.form),
+                        request.form['fluxo_ar'],
+                        request.form['velocidade_tambor'],
+                        request.form['peso'],
+                        request.form['data'],
+                        request.form['observacoes'])
+                    torra.update_torra()
+                    url = '/torra?id=' + request.form['id']
+                    return redirect(url, 302)
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return torras()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
+        return form_login()
 
 @app.route('/compara_torras', methods=['GET','POST']) # compara grafico de torras
 def compara_torras():
     try:
         if logado():
-            if request.form:
-                form = request.form
-                ids = []
-                for i in form:
-                    ids.append(int(i))
-                grid_torras = json_torras(ids)
-                print(grid_torras)
-                path_grafico = 'static/img/graficocomparativo.png'
-                grafico = grafico_torras(grid_torras, path_grafico, ids)
-                randomico = random.randrange(1,1000)  
-                return render_template('compara_torras.html',
-                                grafico = grafico,
-                                ids = ids,
-                                randomico = random.randrange(1,1000))
-            else:
-                return redirect('/torras') 
-                   
-            return render_template(
-                   'compara_torras.html',
-                   torras = select_torras(),
-            )
-    except:
+            try:
+                if request.form:
+                    form = request.form
+                    ids = []
+                    for i in form:
+                        ids.append(int(i))
+                    grid_torras = json_torras(ids)
+                    print(grid_torras)
+                    path_grafico = 'static/img/graficocomparativo.png'
+                    grafico = grafico_torras(grid_torras, path_grafico, ids)
+                    randomico = random.randrange(1,1000)  
+                    return render_template('compara_torras.html',
+                                    grafico = grafico,
+                                    ids = ids,
+                                    randomico = random.randrange(1,1000))
+                else:
+                    return redirect('/torras') 
+
+                return render_template(
+                       'compara_torras.html',
+                       torras = select_torras(),
+                )
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return torras()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
     
 
@@ -291,10 +335,15 @@ def compara_torras():
 def apaga_torra():
     try:
         if logado():
-            id = request.values.get('id')
-            apagar_torra(id)
-            return redirect('/torras',302)
-    except:
+            try:
+                id = request.values.get('id')
+                apagar_torra(id)
+                return redirect('/torras',302)
+            except Exception:
+                logging.exception('ERRO APP CAFES E TORRRAS')
+                return torras()
+    except Exception:
+        logging.exception('ERRO APP CAFES E TORRRAS')
         return form_login()
 
 @app.route('/logoff') # sair do usuário logado
